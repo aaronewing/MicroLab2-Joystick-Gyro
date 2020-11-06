905aa9a22dc1e96dd5ad8ebe128964b289a7088b
aewing7 <72676437+aewing7@users.noreply.github.com>
2020-11-06 13:28:14 -0500

    Add files via upload

---
+++ ctrl_joystick.ino
@@ -0,0 +1,59 @@
+/*================================================================
+ * Lab 2: Game control with Joy Stick, Gyro, and Accelerometer
+ * Controller Used: Joy Stick
+ * Group 100: Sam St.Pierre, Aaron Ewing, Chandara Pheng
+ * EECE.4520 - Microprocessors II
+ * ===============================================================
+ */
+
+
+//Input pins ----------------------------------------------
+const int x_axis = 0;       //Analog pin input
+const int y_axis = 1;       //Analog pin input
+const int sw_pin = 2;       //Digital pin input
+
+//Output pins ---------------------------------------------
+const int buzzer = 4;       //Digital pin output
+int buzz = 0;
+//Signal IO setup -----------------------------------------
+void setup() {
+  pinMode(sw_pin, INPUT);
+  digitalWrite(sw_pin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  //X-axis read
+  if (analogRead(x_axis) > 1000) {
+    //send A
+    Serial.println("d");
+    delay(100);
+  }
+  else if (analogRead(x_axis) < 100) {
+    //send D
+    Serial.println("a");
+    delay(100);
+  }
+
+  //Y-axis read
+  if (analogRead(y_axis) > 1000) {
+    //send S
+    Serial.println("s");
+    delay(100);
+  }
+  else if (analogRead('s') < 100) {
+    //send W
+    Serial.println("w");
+    delay(100);
+  }
+  if (Serial.available() > 0) {
+    buzz = Serial.read();
+
+    if (buzz == '1') {
+      tone(buzzer, 2000);
+      delay(100);
+      noTone(buzzer);
+    }
+  }
+}
